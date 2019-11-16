873975e2d735e006de0937971c0020ef4e40b483
Bartnovskaya Nastya <nastyabartnovskaya@gmail.com>
2019-11-16 14:26:44 +0300

    добавление автодокументирования + унарные операции для вектора

--- task2/Task2/Task2/Vector.cs
+++ task2/Task2/Task2/Vector.cs
@@ -15,20 +15,52 @@ namespace Task2Vector
             this.z = z;
         }
 
-        public static Vector operator + (Vector v1, Vector v2)
+        /// <summary>
+        /// Method for addition vectors
+        /// </summary>
+        /// <param name="v1">First vector for addition</param>
+        /// <param name="v2">Second vector for addition</param>
+        /// <returns></returns>
+        public static Vector operator +(Vector v1, Vector v2)
         {
             return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
         }
 
-        public static Vector operator - (Vector v1, Vector v2)
+        /// <summary>
+        /// Method for subtruction vectors
+        /// </summary>
+        /// <param name="v1">First vector fot subtruction</param>
+        /// <param name="v2">Second vector for subtruction</param>
+        /// <returns></returns>
+        public static Vector operator -(Vector v1, Vector v2)
         {
             return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
         }
 
-        public static Vector operator * (Vector v1, Vector v2)
+        public static Vector operator *(Vector v1, Vector v2)
         {
             return new Vector(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
         }
 
+
+        public static Vector operator -(Vector v)
+        {
+            return new Vector(-v.x, -v.y, -v.z);
+        }
+
+        public static Vector operator ++(Vector v)
+        {
+            return new Vector(++v.x, ++v.y, ++v.z);
+        }
+
+        public static Vector operator --(Vector v)
+        {
+            return new Vector(--v.x, --v.y, --v.z);
+        }
+
+        //public static Vector operator *(Vector v1, Vector v2)
+        //{
+        //    return new Vector(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+        //}
     }
 }

5ac35359a262f10499a6b69ca9a75578f055c52f
Bartnovskaya Nastya <nastyabartnovskaya@gmail.com>
2019-11-16 14:57:03 +0300

    добавление автодокументирования

--- task2/Task2/Task2/Vector.cs
+++ task2/Task2/Task2/Vector.cs
@@ -8,6 +8,12 @@ namespace Task2Vector
         private int y;
         private int z;
 
+        /// <summary>
+        /// Constructor for vector type
+        /// </summary>
+        /// <param name="x">Coordinate x of vector</param>
+        /// <param name="y">Coordinate y of vector</param>
+        /// <param name="z">Coordinate z of vector</param>
         public Vector(int x, int y, int z)
         {
             this.x = x;
@@ -20,7 +26,7 @@ namespace Task2Vector
         /// </summary>
         /// <param name="v1">First vector for addition</param>
         /// <param name="v2">Second vector for addition</param>
-        /// <returns></returns>
+        /// <returns>New vector - result of addition first and second vectors</returns>
         public static Vector operator +(Vector v1, Vector v2)
         {
             return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
@@ -31,36 +37,51 @@ namespace Task2Vector
         /// </summary>
         /// <param name="v1">First vector fot subtruction</param>
         /// <param name="v2">Second vector for subtruction</param>
-        /// <returns></returns>
+        /// <returns>New vector - result vector of subtruction first and second vectors</returns>
         public static Vector operator -(Vector v1, Vector v2)
         {
             return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
         }
 
+        /// <summary>
+        /// Method for multiply vectors
+        /// </summary>
+        /// <param name="v1">First vector for multiply</param>
+        /// <param name="v2">Second vector for multiply</param>
+        /// <returns>New vector - result of multiply first and second vectors</returns>
         public static Vector operator *(Vector v1, Vector v2)
         {
             return new Vector(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
         }
 
-
+        /// <summary>
+        /// Method that change sign of coordinates of vector
+        /// </summary>
+        /// <param name="v">Vector for changing sign of his coordinates</param>
+        /// <returns>New vector - result of changing sign of each coordinates of vector </returns>
         public static Vector operator -(Vector v)
         {
             return new Vector(-v.x, -v.y, -v.z);
         }
 
+        /// <summary>
+        /// Method for adding 1 for each coordinates of vector
+        /// </summary>
+        /// <param name="v">Vector for adding 1 for each coordinates of vector</param>
+        /// <returns>New vector - result of adding 1 for each coordinates of vector</returns>
         public static Vector operator ++(Vector v)
         {
             return new Vector(++v.x, ++v.y, ++v.z);
         }
 
+        /// <summary>
+        /// Method for subtruction 1 for each coordinates of vector
+        /// </summary>
+        /// <param name="v">Vector for subtruction 1 for each coordinates of vector</param>
+        /// <returns>New vector - result of subtruction 1 for each coordinates of vector</returns>
         public static Vector operator --(Vector v)
         {
             return new Vector(--v.x, --v.y, --v.z);
         }
-
-        //public static Vector operator *(Vector v1, Vector v2)
-        //{
-        //    return new Vector(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
-        //}
     }
 }

aba9139bd1871384a990ef44c61ad9da0a7b9e36
Bartnovskaya Nastya <nastyabartnovskaya@gmail.com>
2019-11-23 18:26:36 +0300

    исправление и добавление тестов

--- task2/Task2/Task2/Vector3.cs
+++ task2/Task2/Task2/Vector3.cs
@@ -2,12 +2,12 @@
 
 namespace Task2Vector
 {
-    public class Vector
+    public class Vector3
     {
-        private float x;
-        private float y;
-        private float z;
 
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
 
         /// <summary>
         /// Constructor for integer vector type
@@ -15,11 +15,11 @@ namespace Task2Vector
         /// <param name="x">Integer coordinate x of vector</param>
         /// <param name="y">Integer coordinate y of vector</param>
         /// <param name="z">Integer coordinate z of vector</param>
-        public Vector(float x, float y, float z)
+        public Vector3(float x, float y, float z)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
         }
 
 
@@ -29,9 +29,9 @@ namespace Task2Vector
         /// <param name="v1">First vector for addition</param>
         /// <param name="v2">Second vector for addition</param>
         /// <returns>New vector - result of addition first and second vectors</returns>
-        public static Vector operator + (Vector v1, Vector v2)
+        public static Vector3 operator + (Vector3 v1, Vector3 v2)
         {
-            return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+            return new Vector3(v1.X + v2.X, v1.Y+ v2.Y, v1.Z + v2.Z);
         }
 
         /// <summary>
@@ -40,9 +40,9 @@ namespace Task2Vector
         /// <param name="v1">First vector fot subtruction</param>
         /// <param name="v2">Second vector for subtruction</param>
         /// <returns>New vector - result vector of subtruction first and second vectors</returns>
-        public static Vector operator - (Vector v1, Vector v2)
+        public static Vector3 operator - (Vector3 v1, Vector3 v2)
         {
-            return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+            return new Vector3(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
         }
 
 
@@ -52,12 +52,12 @@ namespace Task2Vector
         /// <param name="v1">First vector for product</param>
         /// <param name="v2">Second vector for product</param>
         /// <returns>New vector - result of vector product first and second vectors</returns>
-        public static Vector operator * (Vector v1, Vector v2)
+        public static Vector3 operator * (Vector3 v1, Vector3 v2)//!
         {
-            float x = v1.y * v2.z - v1.z * v2.y;
-            float y = v1.z * v2.x - v1.x * v2.z;
-            float z = v1.x * v2.y - v1.y * v2.x;
-            return new Vector(x, y, z);
+            float x = v1.Y * v2.Z - v1.Z * v2.Y;
+            float y = v1.Z * v2.X - v1.X * v2.Z;
+            float z = v1.X * v2.Y - v1.Y * v2.X;
+            return new Vector3(x, y, z);
         }
 
         /// <summary>
@@ -66,9 +66,9 @@ namespace Task2Vector
         /// <param name="v1">First vector for scalar product</param>
         /// <param name="v2">Second vector for scalar product</param>
         /// <returns>Number - result of scalar product first and second vectors</returns>
-        public static float ScalarMultiply(Vector v1, Vector v2)
+        public static float ScalarMultiply(Vector3 v1, Vector3 v2) 
         {
-            return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+            return v1.X * v2.X + v1.Y * v2.Y + v1.Z * v2.Z;
         }
 
         /// <summary>
@@ -77,9 +77,9 @@ namespace Task2Vector
         /// <param name="v">Vector for multiply on number</param>
         /// <param name="a">Number for multiply on vector</param>
         /// <returns>New vector - result of multiply vector on integer number</returns>
-        public static Vector operator *(Vector v, int a)
+        public static Vector3 operator *(Vector3 v, int a)
         {
-            return new Vector(v.x * a, v.y * a, v.z * a);
+            return new Vector3(v.X * a, v.Y * a, v.Z * a);
         }
 
         /// <summary>
@@ -88,9 +88,9 @@ namespace Task2Vector
         /// <param name="v">Vector for multiply on number</param>
         /// <param name="a">Number for multiply on vector</param>
         /// <returns>New vector - result of multiply vector on float number</returns>
-        public static Vector operator *(Vector v, float a)
+        public static Vector3 operator *(Vector3 v, float a)
         {
-            return new Vector(v.x * a, v.y * a, v.z * a);
+            return new Vector3(v.X * a, v.Y * a, v.Z * a);
         }
 
         /// <summary>
@@ -98,9 +98,9 @@ namespace Task2Vector
         /// </summary>
         /// <param name="v">Vector for changing sign of his coordinates</param>
         /// <returns>New vector - result of changing sign of each coordinates of vector </returns>
-        public static Vector operator -(Vector v)
+        public static Vector3 operator -(Vector3 v)
         {
-            return new Vector(-v.x, -v.y, -v.z);
+            return new Vector3(-v.X, -v.Y, -v.Z);
         }
 
         /// <summary>
@@ -108,9 +108,9 @@ namespace Task2Vector
         /// </summary>
         /// <param name="v">Vector for adding 1 for each coordinates of vector</param>
         /// <returns>New vector - result of adding 1 for each coordinates of vector</returns>
-        public static Vector operator ++(Vector v)
+        public static Vector3 operator ++(Vector3 v)
         {
-            return new Vector(++v.x, ++v.y, ++v.z);
+            return new Vector3(++v.X, ++v.Y, ++v.Z);
         }
 
         /// <summary>
@@ -118,9 +118,9 @@ namespace Task2Vector
         /// </summary>
         /// <param name="v">Vector for subtruction 1 for each coordinates of vector</param>
         /// <returns>New vector - result of subtruction 1 for each coordinates of vector</returns>
-        public static Vector operator --(Vector v)
+        public static Vector3 operator --(Vector3 v)
         {
-            return new Vector(--v.x, --v.y, --v.z);
+            return new Vector3(--v.X, --v.Y, --v.Z);
         }
 
         /// <summary>
@@ -129,9 +129,9 @@ namespace Task2Vector
         /// <param name="v1">First vector for cheaking inequality with second vector</param>
         /// <param name="v2">Second vector for cheaking inequality with first vector</param>
         /// <returns>True - if vectors are inequa. False - if vectors are not inequal</returns>
-        public static bool operator !=(Vector v1, Vector v2)
+        public static bool operator !=(Vector3 v1, Vector3 v2)
         {
-            if (v1.x != v2.x && v1.y != v2.y && v1.z != v2.z)
+            if (v1.X != v2.X && v1.Y != v2.Y && v1.Z != v2.Z)
             {
                 return true;
             }
@@ -147,9 +147,9 @@ namespace Task2Vector
         /// <param name="v1">First vector for cheaking equality with second vector</param>
         /// <param name="v2">Second vector for cheaking equality with first vector</param>
         /// <returns>True - if vectors are equal. False - if vectors are not equal</returns>
-        public static bool operator == (Vector v1, Vector v2)
+        public static bool operator == (Vector3 v1, Vector3 v2)
         {
-            if(v1.x == v2.x && v1.y == v2.y && v1.z == v2.z)
+            if(v1.X == v2.X && v1.Y == v2.Y && v1.Z == v2.Z)
             {
                 return true;
             }
@@ -165,9 +165,9 @@ namespace Task2Vector
         /// <param name="v1">First vector for compare</param>
         /// <param name="v2">Second vector for compare</param>
         /// <returns>True if the value of the left operand is greater than the right</returns>
-        public static bool operator > (Vector v1, Vector v2)
+        public static bool operator > (Vector3 v1, Vector3 v2)
         {
-            if (v1.x > v2.x && v1.y > v2.y && v1.z > v2.z)
+            if (v1.X > v2.X && v1.Y > v2.Y && v1.Z > v2.Z)
             {
                 return true;
             }
@@ -183,9 +183,9 @@ namespace Task2Vector
         /// <param name="v1">First vector for compare</param>
         /// <param name="v2">Second vector for compare</param>
         /// <returns>True if the value of the left is less than the value of the operand on the right</returns>
-        public static bool operator < (Vector v1, Vector v2)
+        public static bool operator < (Vector3 v1, Vector3 v2)
         {
-            if (v1.x < v2.x && v1.y < v2.y && v1.z < v2.z)
+            if (v1.X < v2.X && v1.Y < v2.Y && v1.Z < v2.Z)
             {
                 return true;
             }
@@ -201,9 +201,9 @@ namespace Task2Vector
         /// <param name="v1">First vector for compare</param>
         /// <param name="v2">Second vector for compare</param>
         /// <returns>True if the value of the left is less or equal than the value of the operand on the right</returns>
-        public static bool operator <= (Vector v1, Vector v2)
+        public static bool operator <= (Vector3 v1, Vector3 v2)
         {
-            if (v1.x <= v2.x && v1.y <= v2.y && v1.z <= v2.z)
+            if (v1.X <= v2.X && v1.Y <= v2.Y && v1.Z <= v2.Z)
             {
                 return true;
             }
@@ -219,9 +219,9 @@ namespace Task2Vector
         /// <param name="v1">First vector for compare</param>
         /// <param name="v2">Second vector for compare</param>
         /// <returns>True if the value of the left is less or equal than the value of the operand on the right</returns>
-        public static bool operator >= (Vector v1, Vector v2)
+        public static bool operator >= (Vector3 v1, Vector3 v2)
         {
-            if (v1.x >= v2.x && v1.y >= v2.y && v1.z >= v2.z)
+            if (v1.X >= v2.X && v1.Y >= v2.Y && v1.Z >= v2.Z)
             {
                 return true;
             }
---
+++ task2/Task2/UnitTestsVector/UnitTestVector3.cs
@@ -0,0 +1,62 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Task2Vector;
+
+namespace UnitTestsTask2
+
+{
+    [TestClass]
+    public class UnitTestVector3
+    {
+        private Vector3 CheckVectorOnNumber(Vector3 v1, float a)
+        {
+            Vector3 result = v1 * a;
+
+            if (result.X / a == v1.X && result.Y / a == v1.Y && result.Z / a == v1.Z)
+            {
+                return result;
+            }
+            else
+            {
+                return v1;
+            }
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod()
+        {
+
+            Vector3 v1;
+            Vector3 v2;
+            Vector3 result;
+            float scalarResult;
+            Random random = new Random();
+
+            for(int i = 0; i < 50; i++)
+            {
+                v1 = new Vector3(random.Next(-1550, 1254), random.Next(-568, 2), random.Next(10, 154879));
+                v2 = new Vector3(random.Next(-10, 5000), random.Next(-788, 20), random.Next(100, 152139));
+                result = v1 + v2;
+                Assert.IsTrue(v1 == result - v2);
+                result = v1 - v2;
+                Assert.IsTrue(v1 == result + v2);
+                result = v1 * 5;
+                Assert.IsTrue(result == CheckVectorOnNumber(v1, 5));
+                result = ++result;
+                Assert.IsTrue(result == result--);
+            }
+
+            v1 = new Vector3(12, 125, -45);
+            v2 = new Vector3(-8, 78, 90);
+            result = new Vector3(14760, -720, 1936);
+            Assert.IsTrue((v1 * v2) == result);
+
+
+            scalarResult = Vector3.ScalarMultiply(v1, v2);
+            Assert.AreEqual(5604, scalarResult);
+
+        }
+    }
+}

6cf59193982a6e328d061edfbff000c86b743ae6
Bartnovskaya Nastya <nastyabartnovskaya@gmail.com>
2019-11-24 15:10:16 +0300

    создание класса дл работы с полиномом

---
+++ task2/Task2/Polinom/Polynom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Polynom
+{
+    public class Polynom
+    {
+        int Length
+        {
+            get { return Length; }
+            set
+            {
+                if (Length > 0)
+                {
+                    Length = value;
+                }
+            }
+        }
+
+        int[] coefficients;
+        int[] degrees;
+
+        public Polynom (int length)
+        {
+
+        }
+
+
+
+
+
+    }
+}

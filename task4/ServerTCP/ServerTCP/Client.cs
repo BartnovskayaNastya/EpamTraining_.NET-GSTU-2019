879f636a597c779496510354bbd8eb852b520aed
Bartnovskaya Nastya <nastyabartnovskaya@gmail.com>
2019-12-21 21:57:17 +0300

    add client and server classes

---
+++ task4/ServerTCP/ServerTCP/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerClientTCP
+{
+    class Client
+    {
+        public string IpAdress { get; private set; }
+
+        public int Port { get; private set; }
+        private IPEndPoint endPoint;
+        private Socket socketTcp;
+
+        public Client(int port = 8079, string ipAdress = "127.0.0.1")
+        {
+            Port = port;
+            IpAdress = ipAdress;
+            endPoint = new IPEndPoint(IPAddress.Parse(ipAdress), port);
+            socketTcp = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void GetMsg(string msg)
+        {
+            var data = Encoding.UTF8.GetBytes(msg);
+            socketTcp.Connect(endPoint);
+            socketTcp.Send(data);
+
+            byte[] buffer = new byte[1024];
+            var size = 0;
+            StringBuilder serverAnswer = new StringBuilder();
+
+            do
+            {
+                size = socketTcp.Receive(buffer);
+                serverAnswer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+
+            }
+            while (socketTcp.Available > 0);
+
+            socketTcp.Shutdown(SocketShutdown.Both);
+            socketTcp.Close();
+
+        }
+    }
+}
---
+++ task4/ServerTCP/ServerTCP/Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerTCP
+{
+    public class Server
+    {
+        public string IpAdress { get; private set; }
+       
+        public int Port { get; private set; }
+        private IPEndPoint endPoint;
+        private Socket socketTcp;
+
+        public Server(int port = 8079, string ipAdress = "127.0.0.1")
+        {
+            Port = port;
+            IpAdress = ipAdress;
+            endPoint = new IPEndPoint(IPAddress.Parse(ipAdress), port);
+            socketTcp = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socketTcp.Bind(endPoint);
+        }
+
+        public void ListenClients(int count)
+        {
+            socketTcp.Listen(count);
+            while (true)
+            {
+                Socket listener = socketTcp.Accept();
+                byte[] buffer = new byte[1024];
+                var size = 0;
+                StringBuilder data = new StringBuilder();
+
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+
+                }
+                while (listener.Available > 0);
+
+                byte[] msg = Encoding.UTF8.GetBytes("Successfully");
+                listener.Send(msg);
+
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+
+
+        }
+       
+
+        
+
+        
+
+
+    }
+}

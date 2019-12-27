f7bf72c8ef88a1153ac1adf7d4d72138c6d145ef
Bartnovskaya Nastya <nastyabartnovskaya@gmail.com>
2019-12-27 15:41:57 +0300

    add student class

---
+++ task5/Students/Students/Student.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Students
+{
+    public class Student : IComparable<Student>
+    {
+        private string name;
+        private string testName;
+        private int mark;
+        public string Name 
+        {
+            get 
+            {
+                return name;
+            } 
+            set
+            {
+                if(name.Length < 1)
+                {
+                    throw new Exception("You need to input some name");
+                }
+
+                name = value;
+            }
+        } 
+
+        public string TestName 
+        {
+            get
+            {
+                return testName;
+            }
+            set
+            {
+                if (testName.Length < 1)
+                {
+                    throw new Exception("You need to input some name");
+                }
+
+                testName = value;
+            }
+        }
+
+        public DateTime dateTest;
+        public int Mark
+        {
+            get
+            {
+                return mark;
+            }
+            set
+            {
+                if (mark < 1 || mark > 10)
+                {
+                    throw new Exception("You need to input mark between 1 and 10");
+                }
+
+                mark = value;
+            }
+        }
+
+        public Student(string name, string testName, DateTime dateTest, int mark)
+        {
+            Name = name;
+            TestName = testName;
+            this.dateTest = dateTest;
+            Mark = mark;
+
+        }
+
+        public int CompareTo(Student student)
+        {
+            if (student != null)
+                return this.Mark.CompareTo(student.Mark);
+            else
+                throw new Exception("Impossible to compare this students");
+        }
+    }
+}
